e56f5e1768cb6a8c228d896ce9926653c0c42141
Jakub Pomykała <jakub.pomykala@gmail.com>
2024-06-04 18:24:25 +0200

    Increase timeout and add maven

--- src/main/java/com/jpomykala/cachepopulatejava/BrowserContextRunner.java
+++ src/main/java/com/jpomykala/cachepopulatejava/BrowserContextRunner.java
@@ -4,7 +4,7 @@ import com.microsoft.playwright.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -15,55 +15,19 @@ public class BrowserContextRunner {
 
     public void run(String domain, Consumer<BrowserContext> contextSupplier) {
         try (Playwright playwright = Playwright.create()) {
-            try (Browser browser = playwright.chromium().launch()) {
-                try (BrowserContext context = browser.newContext()) {
-                    context.setDefaultTimeout(10_000);
-                    context.setDefaultNavigationTimeout(10_000);
-                    context.onResponse(onResponseConsumer(domain));
-                    context.onRequestFailed(onRequestFailedConsumer(domain));
-                    contextSupplier.accept(context);
+            List<BrowserType> browserTypes = List.of(playwright.chromium());
+            for (BrowserType browserType : browserTypes) {
+                try (Browser browser = browserType.launch()) {
+                    try (BrowserContext context = browser.newContext()) {
+                        context.setDefaultTimeout(30_000);
+                        context.setDefaultNavigationTimeout(30_000);
+                        context.onRequestFailed(onRequestFailedConsumer(domain));
+                        contextSupplier.accept(context);
+                    }
                 }
             }
         }
     }
-
-    private static Consumer<Response> onResponseConsumer(String domain) {
-        return response -> {
-            String url = response.url();
-            if (!url.contains(domain)) {
-                return;
-            }
-            Map<String, String> headers = response.headers();
-            String cacheControl = headers.get("cache-control");
-            if (cacheControl != null && cacheControl.contains("private")) {
-                log.warn("Private cache control: {}", url);
-            }
-            if (cacheControl != null && cacheControl.contains("no-cache")) {
-                log.warn("No cache control: {}", url);
-            }
-
-            if (cacheControl != null && cacheControl.contains("no-store")) {
-                log.warn("No store cache control: {}", url);
-            }
-
-            if (cacheControl != null && cacheControl.contains("max-age=0")) {
-                log.warn("Max age 0 cache control: {}", url);
-            }
-
-            if (cacheControl != null && cacheControl.contains("must-revalidate")) {
-                log.warn("Must revalidate cache control: {}", url);
-            }
-
-            if (cacheControl != null && cacheControl.contains("proxy-revalidate")) {
-                log.warn("Proxy revalidate cache control: {}", url);
-            }
-
-            if (cacheControl == null) {
-                log.warn("No cache control: {}", url);
-            }
-        };
-    }
-
 
     private Consumer<Request> onRequestFailedConsumer(String domain) {
         return request -> {
