99c1a52fbc540594a810bd1abfa18daddf674257
Jakub Pomykała <jakub.pomykala@gmail.com>
2024-09-06 12:36:19 +0200

    Minor adjustments to prevent exiting with status 1

--- src/main/java/com/jpomykala/cachepopulatejava/BrowserContextRunner.java
+++ src/main/java/com/jpomykala/cachepopulatejava/BrowserContextRunner.java
@@ -5,52 +5,66 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 
 @Slf4j
 @Component
-public class BrowserContextRunner {
+public class BrowserContextRunner
+{
 
 
-    public void run(String domain, Consumer<BrowserContext> contextSupplier) {
-        try (Playwright playwright = Playwright.create()) {
-            List<BrowserType> browserTypes = List.of(playwright.chromium());
-            for (BrowserType browserType : browserTypes) {
-                try (Browser browser = browserType.launch()) {
-                    try (BrowserContext context = browser.newContext()) {
-                        context.setDefaultTimeout(30_000);
-                        context.setDefaultNavigationTimeout(30_000);
-                        context.onRequestFailed(onRequestFailedConsumer(domain));
-                        contextSupplier.accept(context);
-                    }
-                }
-            }
+  public void run(String domain, Consumer<BrowserContext> contextSupplier)
+  {
+    try (Playwright playwright = Playwright.create())
+    {
+      List<BrowserType> browserTypes = List.of(playwright.chromium());
+      for (BrowserType browserType : browserTypes)
+      {
+        try (Browser browser = browserType.launch())
+        {
+          try (BrowserContext context = browser.newContext())
+          {
+            context.setDefaultTimeout(60_000);
+            context.setDefaultNavigationTimeout(60_000);
+            context.setExtraHTTPHeaders(Map.of(
+                    "X-Cache-Populate", "true"
+            ));
+            context.onRequestFailed(onRequestFailedConsumer(domain));
+            contextSupplier.accept(context);
+          }
         }
+      }
     }
+  }
 
-    private Consumer<Request> onRequestFailedConsumer(String domain) {
-        return request -> {
-            String url = request.url();
+  private Consumer<Request> onRequestFailedConsumer(String domain)
+  {
+    return request -> {
+      String url = request.url();
 
-            if (!url.contains(domain)) {
-                return;
-            }
+      if (!url.contains(domain))
+      {
+        return;
+      }
 
-            Optional<Integer> statusCode = Optional.of(request)
-                    .map(Request::response)
-                    .map(Response::status);
-            if (statusCode.isPresent() && statusCode.get() >= 200 && statusCode.get() <= 299) {
-                return;
-            }
+      Optional<Integer> statusCode = Optional.of(request)
+              .map(Request::response)
+              .map(Response::status);
+      if (statusCode.isPresent() && statusCode.get() >= 200 && statusCode.get() <= 299)
+      {
+        return;
+      }
 
-            if (statusCode.isPresent()) {
-                log.error("- Request failed ({}): {}", request.response().status(), url);
-                return;
-            }
+      if (statusCode.isPresent())
+      {
+        log.error("- Request failed ({}): {}", request.response().status(), url);
+        return;
+      }
 
-            log.error("- Request failed: {} -> {}", request.failure(), url);
-        };
-    }
+      log.error("- Request failed: {} -> {}", request.failure(), url);
+    };
+  }
 
 }
--- src/main/java/com/jpomykala/cachepopulatejava/PopulateCacheCommand.java
+++ src/main/java/com/jpomykala/cachepopulatejava/PopulateCacheCommand.java
@@ -19,109 +19,106 @@ import java.util.List;
 @Slf4j
 @ShellComponent
 @RequiredArgsConstructor
-public class PopulateCacheCommand {
+public class PopulateCacheCommand
+{
 
-    private final ExportService exportService;
-    private final SitemapFetcher sitemapFetcher;
-    private final ScreenshotService screenshotService;
-    private final BrowserContextRunner browserContextRunner;
+  private final ExportService exportService;
+  private final SitemapFetcher sitemapFetcher;
+  private final ScreenshotService screenshotService;
+  private final BrowserContextRunner browserContextRunner;
 
-    @SneakyThrows
-    @ShellMethod(key = "fetch", value = "Fetch all urls from sitemap to populate cache")
-    public void fetch(
-            @ShellOption final String sitemap,
-            @ShellOption(defaultValue = "false") final boolean screenshots,
-            @ShellOption(defaultValue = "") final String exportFormat,
-            @ShellOption(defaultValue = "-1") final int maxPages
-    ) {
+  @SneakyThrows
+  @ShellMethod(key = "fetch", value = "Fetch all urls from sitemap to populate cache")
+  public void fetch(@ShellOption final String sitemap, @ShellOption(defaultValue = "false") final boolean screenshots, @ShellOption(defaultValue = "") final String exportFormat, @ShellOption(defaultValue = "-1") final int maxPages)
+  {
 
-        log.info("Starting cache population for sitemap: {}", sitemap);
+    try
+    {
+      log.info("Starting cache population for sitemap: {}", sitemap);
 
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("## Cache population\n");
+      StringBuilder stringBuilder = new StringBuilder();
+      stringBuilder.append("## Cache population\n");
 
-        final String domain = DomainUtils.getDomain(sitemap);
-        final List<String> sitemapUrls = sitemapFetcher.fetchAllSiteMapUrls(sitemap);
-        log.info("Found {} urls in sitemap", sitemapUrls.size());
+      final String domain = DomainUtils.getDomain(sitemap);
+      final List<String> sitemapUrls = sitemapFetcher.fetchAllSiteMapUrls(sitemap);
+      log.info("Found {} urls in sitemap", sitemapUrls.size());
 
-        stringBuilder.append("Sitemap: ").append(sitemap).append("\n");
-        stringBuilder.append("Found ").append(sitemapUrls.size()).append(" urls\n");
-        if (maxPages > 0) {
-            stringBuilder.append("Max pages: ").append(maxPages).append("\n");
+      stringBuilder.append("Sitemap: ").append(sitemap).append("\n");
+      stringBuilder.append("Found ").append(sitemapUrls.size()).append(" urls\n");
+      if (maxPages > 0)
+      {
+        stringBuilder.append("Max pages: ").append(maxPages).append("\n");
+      }
+
+
+      List<ErroredPage> erroredPages = new ArrayList<>();
+      browserContextRunner.run(domain, context -> {
+        int visitedPagesCounter = 0;
+        Duration totalTime = Duration.ZERO;
+        List<VisitedPage> visitedPages = new ArrayList<>();
+
+        for (String url : sitemapUrls)
+        {
+          if (maxPages > 0 && visitedPagesCounter > maxPages)
+          {
+            log.info("Max pages limit reached");
+            break;
+          }
+
+          var start = System.currentTimeMillis();
+          try
+          {
+            Page page = context.newPage();
+            page.navigate(url);
+            page.waitForLoadState(LoadState.LOAD);
+
+            var end = System.currentTimeMillis();
+            if (screenshots)
+            {
+              screenshotService.takeScreenshot(page);
+            }
+
+            visitedPages.add(VisitedPage.builder().url(url).title(page.title()).build());
+
+            page.close();
+            visitedPagesCounter++;
+            var percentage = (float) visitedPagesCounter / sitemapUrls.size() * 100;
+            var percentageFormatted = String.format("%.2f", percentage);
+            var timeElapsed = end - start;
+            totalTime = totalTime.plusMillis(timeElapsed);
+            log.info("({}%) Loaded ({}ms) {}", percentageFormatted, timeElapsed, url);
+          } catch (Exception e)
+          {
+            log.error("Error loading page: {}", url, e);
+            erroredPages.add(ErroredPage.builder().url(url).error(e.getMessage()).build());
+          }
         }
 
+        long totalTimeMillis = totalTime.toMillis();
+        String totalTimeString = DurationFormatUtils.formatDurationWords(totalTimeMillis, true, true);
+        Duration avgDuration = totalTime.dividedBy(visitedPagesCounter);
+        long avgDurationMillis = avgDuration.toMillis();
+        log.info("Visited {} urls in {} (avg {}ms)", visitedPagesCounter, totalTimeString, avgDurationMillis);
 
-        List<ErroredPage> erroredPages = new ArrayList<>();
-        browserContextRunner.run(domain, context -> {
-            int visitedPagesCounter = 0;
-            Duration totalTime = Duration.ZERO;
-            List<VisitedPage> visitedPages = new ArrayList<>();
+        stringBuilder.append("Visited ").append(visitedPagesCounter).append(" urls in ").append(totalTimeString).append(" (avg ").append(avgDurationMillis).append("ms)\n");
+        if (!erroredPages.isEmpty())
+        {
+          stringBuilder.append("## Errors\n");
+          erroredPages.forEach(erroredPage -> {
+            log.warn("Error loading page: {} - {}", erroredPage.url(), erroredPage.error());
+            stringBuilder.append("- ").append(erroredPage.url()).append(" - ").append(erroredPage.error()).append("\n");
+          });
+        }
 
-            for (String url : sitemapUrls) {
-                if (maxPages > 0 && visitedPagesCounter > maxPages) {
-                    log.info("Max pages limit reached");
-                    break;
-                }
-
-                var start = System.currentTimeMillis();
-                Page page;
-                try {
-                    page = context.newPage();
-                    page.navigate(url);
-                    page.waitForLoadState(LoadState.LOAD);
-                } catch (Exception e) {
-                    log.error("Error loading page: {}", url, e);
-                    erroredPages.add(ErroredPage.builder()
-                            .url(url)
-                            .error(e.getMessage())
-                            .build()
-                    );
-                    continue;
-                }
-
-                var end = System.currentTimeMillis();
-                if (screenshots) {
-                    screenshotService.takeScreenshot(page);
-                }
-
-                visitedPages.add(VisitedPage.builder()
-                        .url(url)
-                        .title(page.title())
-                        .build()
-                );
-
-                page.close();
-                visitedPagesCounter++;
-                var percentage = (float) visitedPagesCounter / sitemapUrls.size() * 100;
-                var percentageFormatted = String.format("%.2f", percentage);
-                var timeElapsed = end - start;
-                totalTime = totalTime.plusMillis(timeElapsed);
-                log.info("({}%) Loaded ({}ms) {}", percentageFormatted, timeElapsed, url);
-            }
-
-            long totalTimeMillis = totalTime.toMillis();
-            String totalTimeString = DurationFormatUtils.formatDurationWords(totalTimeMillis, true, true);
-            Duration avgDuration = totalTime.dividedBy(visitedPagesCounter);
-            long avgDurationMillis = avgDuration.toMillis();
-            log.info("Visited {} urls in {} (avg {}ms)",
-                    visitedPagesCounter,
-                    totalTimeString,
-                    avgDurationMillis
-            );
-
-            stringBuilder.append("Visited ").append(visitedPagesCounter).append(" urls in ").append(totalTimeString).append(" (avg ").append(avgDurationMillis).append("ms)\n");
-            if (!erroredPages.isEmpty()) {
-                stringBuilder.append("## Errors\n");
-                erroredPages.forEach(erroredPage -> {
-                    log.error("Error loading page: {} - {}", erroredPage.url(), erroredPage.error());
-                    stringBuilder.append("- ").append(erroredPage.url()).append(" - ").append(erroredPage.error()).append("\n");
-                });
-            }
-
-            GithubActionSummaryUtil.writeSummary(stringBuilder.toString());
-            if (StringUtils.hasText(exportFormat)) {
-                exportService.export(visitedPages, exportFormat);
-            }
-        });
+        GithubActionSummaryUtil.writeSummary(stringBuilder.toString());
+        if (StringUtils.hasText(exportFormat))
+        {
+          exportService.export(visitedPages, exportFormat);
+        }
+      });
+    } catch (Exception e)
+    {
+      log.warn("Error populating cache", e);
     }
+  }
 }
